5c11a041901473ae9e7d9a4ad91773ceb08f6d61
oUwLe <sebastian.ljung18@hotmail.com>
2025-06-04 16:52:41 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,12 @@
-//Variables to use and it's a mix of global and local variables
-let shoppingCart = [];
-let products = document.getElementsByTagName('button');
-let cartProducts = '';
+/**
+ * PROBLEM:
+ * Lägger inte till saker när man trycker på "Add to cart"
+ * Visar inte vad som är i "Cart" när man trycker på den
+ * ---
+ * PSEUDO:
+ * 1. Hämta alla knapp-element i JS och spara i en variabel
+ * 2. Loopa igenom alla knapp-element och sätta en eventlistener på varje
+ * 3. Deklarera en variabel som är en array som är vår varukorg
+ * 4. 
+ */
 
-//This code gets whatever is inside data-product in the HTML
-let product = event.target.parentNode.getAttribute('data-product');
-cartProducts = cartProducts + '<li><span class="product-title">Titel: </span>' + shoppingCart[i] + '</li>';
-shoppingCart.push(product);
-
-//
-document.querySelector('#productsInCart').innerHTML = shoppingCart.length;
-document.querySelector('#cart').classList.toggle('hide');
-document.querySelector('#products').innerHTML = cartProducts;
-products[i].addEventListener('click', (event) => {
-  console.log(event.target);
-  // event here is the HTML element that was clicked on
-});
-document.querySelector('#open-cart').addEventListener('click', () => {});
-
-//Function declarations, add code inside {}
-function updateCart() {}
-function listProductsInCart() {}
-
-
-//For-loops of two arrays
-for(let i = 0; i < shoppingCart.length; i++) {}
-for(let i = 0; i < products.length; i++) {}
-
-
-//Function calls and there should be two listProductsInCart()
-updateCart();
-listProductsInCart();
-listProductsInCart();
